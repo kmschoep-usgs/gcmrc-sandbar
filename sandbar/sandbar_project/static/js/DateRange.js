835ad0961845f7d5f74e52cf7e7d45cd20f24c70
Mary Bucknell <mbucknell@usgs.gov>
2013-11-13 14:24:01 +0000

    I was not using the jquery validate plug in properly for use with ajax. Fixed that and now it works properly in Firefox. Still need to test in IE9.

--- sandbar/sandbar_project/static/js/DateRange.js
+++ sandbar/sandbar_project/static/js/DateRange.js
@@ -2,24 +2,25 @@ SB.DateRange = function(startEl, endEl) {
 	this.startEl = startEl;
 	this.endEl = endEl;
 	
-	this.startEl.val(SB.Config.SITE_INITIAL_START_DATE).prop('min', SB.Config.SITE_MIN_DATE).prop('max', SB.Config.SITE_MAX_DATE);
-	this.endEl.val(SB.Config.SITE_INITIAL_END_DATE).prop('min', SB.Config.SITE_MIN_DATE).prop('max', SB.Config.SITE_MAX_DATE);
+	this.startEl.val(SB.Config.SITE_INITIAL_START_DATE).attr('min', SB.Config.SITE_MIN_DATE).attr('max', SB.Config.SITE_MAX_DATE);
+	this.endEl.val(SB.Config.SITE_INITIAL_END_DATE).attr('min', SB.Config.SITE_MIN_DATE).attr('max', SB.Config.SITE_MAX_DATE);
 	
 	this.startEl.bind('blur', $.proxy(function() {
 		var startVal = $(this.startEl).val();
-		if (this.startEl.prop('max') >= startVal) {
-			this.endEl.prop('min', startVal);
-			return false;
+		if (this.startEl.attr('max') >= startVal) {
+			this.endEl.attr('min', startVal);
 		}
+		return false;
 		
 	}, this));
 	
 	this.endEl.bind('blur', $.proxy(function() {
 		var endVal = $(this.endEl).val();
-		if(this.endEl.prop('min') <= endVal) {
-			this.startEl.prop('max', endVal);
-			return false;
+		if(this.endEl.attr('min') <= endVal) {
+			this.startEl.attr('max', endVal);
 		}
+		return false;
+
 	}, this));
 	
 };
