dc25038a0306ea250ded53ce6c2500c1108722f7
Yan, Andrew N. <ayan@usgs.gov>
2014-06-30 11:31:19 -0500

    renamed method on JSON mixin to be an internal method

--- sandbar/surveys/custom_mixins.py
+++ sandbar/surveys/custom_mixins.py
@@ -40,12 +40,12 @@ class JSONResponseMixin(object):
     
     def render_to_json_response(self, context, **response_kwargs):
         
-        context_json = self.convert_context_to_json(context)
+        context_json = self._convert_context_to_json(context)
         
         return HttpResponse(context_json, content_type="application/json", **response_kwargs)
     
     
-    def convert_context_to_json(self, context):
+    def _convert_context_to_json(self, context):
         
         json_dump = json.dumps(context)
         
