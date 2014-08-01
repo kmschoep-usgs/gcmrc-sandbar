b9f8ff48d776a7235d687db81685292090a2a788
Yan, Andrew N. <ayan@usgs.gov>
2014-08-01 11:48:27 -0500

    1. updated requirements 2. re-added SQLAlchemy class

--- sandbar/surveys/db_utilities.py
+++ sandbar/surveys/db_utilities.py
@@ -1,7 +1,44 @@
+from sqlalchemy import create_engine, func
+from sqlalchemy.sql.functions import GenericFunction
+from sqlalchemy.orm import sessionmaker
 
 def convert_datetime_to_str(date_object, date_format='%Y-%m-%d'):
+    
+    """
+    Convert a python date object to a string.
+    """
     
     date_str = date_object.strftime(date_format)
     
     return date_str
+
+class QueryDB(object): 
+    
+    def __init__(self, schema, password, db_name):
+        
+        self.connect = 'oracle+cx_oracle://%s:%s@%s' % (schema, password, db_name)
+        self.engine = create_engine(self.connect)
+               
+    def create_session(self):
+        
+        Session = sessionmaker()
+        Session.configure(bind=self.engine)
+        session = Session()
+        
+        return session
+    
+if __name__ == '__main__':
+    
+    from sandbar_project.local_settings import SCHEMA_USER, DB_PWD, DB_DESC
+    from db_mappings import AreaVolumeCalcBase
+    
+    q = QueryDB(SCHEMA_USER, DB_PWD, DB_DESC)
+    session = q.create_session()
+    dates = session.query(AreaVolumeCalcBase.calc_date).distinct(AreaVolumeCalcBase.calc_date).filter(func.lower(AreaVolumeCalcBase.calc_type)=='eddy').filter(AreaVolumeCalcBase.site_id=='38').order_by(AreaVolumeCalcBase.calc_date)
+    for date in dates:
+        print date
+        d1 = session.query(AreaVolumeCalcBase.plane_height, AreaVolumeCalcBase.next_plane_height, AreaVolumeCalcBase.area_2d_amt).filter(AreaVolumeCalcBase.prev_plane_height!=0).order_by(AreaVolumeCalcBase.plane_height)
+        d1_list = d1.all()
+        print d1_list[0].plane_height
+    session.close()
         
