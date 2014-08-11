135f19eed6130b2c44dc7ee4c3084813c125af6b
Tomcat Application Server <tomcat@cida-eros-sbdjdev.er.usgs.gov>
2014-08-11 15:53:17 -0500

    automatic inclusion of the env and static files

---
+++ sandbar/static/js/GDAWSFormatUtils.js
@@ -0,0 +1,24 @@
+SB.GDAWSFormatUtils = {
+	getDygraphCSV : function(jsonResp, dataKey, dataLabel) {
+		var LF = '\n';
+		var result = 'Time,' + dataLabel + LF;
+		
+		var data = jsonResp.success.data;
+		
+		for (var i = 0; i < data.length; i++) {
+			result += data[i].time + ',' + data[i][dataKey] + LF;
+		}
+		
+		return result;
+	},
+	getDataQueryString : function(gdawsSiteId, startDate, endDate, params) {
+		var paramsData = '';
+		for (var i = 0; i < params.length; i++) {
+			var thisConfig = SB.Config.SITE_PARAMETERS[params[i]];
+			paramsData += '&column[]=inst!' + thisConfig.groupName +  '!' + gdawsSiteId + '!' + thisConfig.colName;
+		}
+		return 'beginPosition=' + startDate + '&endPosition=' + endDate + 
+			'&column[]=time!ISO!time' + paramsData +
+			'&every=P1M&tz=-7&noDataFilter=true&output=json&downscale=PT6H';
+	}
+}
