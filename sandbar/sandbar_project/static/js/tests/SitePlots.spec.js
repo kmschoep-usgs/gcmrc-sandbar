ba0eb7691dbd114b366559f8410cb32132b9998a
Yan, Andrew N. <ayan@usgs.gov>
2014-08-11 14:52:46 -0500

    more experimenting

--- sandbar/sandbar_project/static/js/tests/SitePlots.spec.js
+++ sandbar/sandbar_project/static/js/tests/SitePlots.spec.js
@@ -60,7 +60,6 @@ describe('Test SitePlots', function() {
 		beforeEach(function() {
 			sitePlots = new SB.SitePlots('graphs-div', '1367');
 			//tsPlots = new SB.TSPlots('graphs-div', '2468');
-			var fakeTsPlots = {area6d: 'blah'}
 			SB.GDAWS_SERVICE = 'http://fakegdaws/service/';
 			
 			xhr = sinon.useFakeXMLHttpRequest();
@@ -75,6 +74,7 @@ describe('Test SitePlots', function() {
 		});
 		
 		it('Expects loading div to be visible before ajax call completes', function() {
+			var fakeTsPlots = {area6d: 'blah'}
 			sitePlots.updatePlots('2013-04-01', '2013-04-04', ['P2', 'P3'], fakeTsPlots, ['P2', 'P3', 'P4', 'P5']);
 			expect($('#graphs-loading-div').is(':visible')).toBe(undefined);
 		});
