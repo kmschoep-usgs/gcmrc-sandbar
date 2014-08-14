e1298d02cc6f1daa9485f47d13591b92b4e070d7
Yan, Andrew N. <ayan@usgs.gov>
2014-08-14 11:05:06 -0500

    modified behavior of discharge and sand load graphs -- if no sandbar is available, they will default to their min and max dates.

--- sandbar/sandbar_project/static/js/SitePageOnReady.js
+++ sandbar/sandbar_project/static/js/SitePageOnReady.js
@@ -5,7 +5,8 @@ SB.SitePageOnReady = function(gdawsSiteId, siteId) {
 	for (key in SB.Config.SITE_PARAMETERS) {
 		queryParams += '&groupName=' + SB.Config.SITE_PARAMETERS[key].groupName;
 	}
-	
+	var gcmrcStartDate;
+	var gcmrcEndDate;
 	// Fetch the parameter display information from GCMRC
 	$.ajax({
 		url: SB.GDAWS_SERVICE + 'service/param/json/param/', 
@@ -56,7 +57,8 @@ SB.SitePageOnReady = function(gdawsSiteId, siteId) {
 					// Update the dateRange limits and set initial Dates.
 					minDateText = minDate.toISOString().slice(0,10);
 					maxDateText = maxDate.toISOString().slice(0,10);
-					
+					gcmrcStartDate = minDateText;
+					gcmrcEndDate = maxDateText;
 					dateRange = new SB.DateRange($('#start-date'), $('#end-date'), {
 						initialEnd : maxDateText,
 						minDate: minDateText,
@@ -233,7 +235,9 @@ SB.SitePageOnReady = function(gdawsSiteId, siteId) {
 			if (errorExists === 0 && clickTrigger != "download-data") {
 				$('#parameter-errors').html('');
 				$('#parameter-errors').hide();
-				sitePlots.updatePlots(sandbarStartDate, sandbarEndDate, gcmrcParams, tsPlots._graphs, params);
+				var plotStartDate = createPlotDates(sandbarStartDate, gcmrcStartDate);
+				var plotEndDate = createPlotDates(sandbarEndDate, gcmrcEndDate);
+				sitePlots.updatePlots(plotStartDate, plotEndDate, gcmrcParams, tsPlots._graphs, params);
 				tsPlots.updatePlots($('#ds-min').val(), $('#ds-max').val(), subParam, sitePlots._graphs, params);
 				}
 
--- sandbar/sandbar_project/static/js/utils.js
+++ sandbar/sandbar_project/static/js/utils.js
@@ -3,3 +3,16 @@ function get_value(element_id) {
 	var elementValue = $(element_id).val();
 	return elementValue;
 };
+
+function createPlotDates(dateStr, altDateStr) {
+	if (dateStr != null) {
+		plotDate = dateStr;
+	}
+	else if (altDateStr != null) {
+		plotDate = altDateStr;
+	}
+	else {
+		plotDate = null;
+	}
+	return plotDate;
+}
