d5524d3fa5a5932a7e5983b030213218587d3266
kmschoep <kmschoep@usgs.gov>
2014-08-15 12:01:51 -0500

    parameter title change

--- sandbar/surveys/views.py
+++ sandbar/surveys/views.py
@@ -325,8 +325,8 @@ class BasicSiteInfoJSON(JSONResponseMixin, View):
         site_info = {'siteID': site_id,
                      'calcDates': {'min': area_min_date_str,
                                   'max': area_max_date_str,},
-                     'paramNames': {'area2d': 'Area 2D',
-                                    'volume': 'Volume'}
+                     'paramNames': {'area2d': 'Area of sandbar between lower and upper bound',
+                                    'volume': 'Volume of sandbar between lower and upper bound'}
                      }
         return self.render_to_json_response(site_info)
     
