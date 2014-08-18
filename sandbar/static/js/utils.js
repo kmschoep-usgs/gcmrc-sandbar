e835cf5a861c90977e03e7a4dc4e2cb3099b001e
Tomcat Application Server <tomcat@cida-eros-sbdjdev.er.usgs.gov>
2014-08-18 10:27:01 -0500

    automatic inclusion of the env and static files

---
+++ sandbar/static/js/utils.js
@@ -0,0 +1,29 @@
+// get the value of an element
+function get_value(element_id) {
+	var elementValue = $(element_id).val();
+	return elementValue;
+};
+
+function createPlotDates(dateStr, altDateStr) {
+	if (dateStr != null) {
+		plotDate = dateStr;
+	}
+	else if (altDateStr != null) {
+		plotDate = altDateStr;
+	}
+	else {
+		plotDate = null;
+	}
+	return plotDate;
+};
+
+function collect() {
+	var ret = {};
+	var len = arguments.length;
+	for (var i = 0; i < len; i++) {
+		for (p in arguments[i]) {
+			ret[p] = arguments[i][p]
+		}
+	}
+	return ret;
+}
