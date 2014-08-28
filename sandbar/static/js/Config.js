cad325d01bc7e81a074c3cdbfb90305d19c7037d
Tomcat Application Server <tomcat@cida-eros-sbdjdev.er.usgs.gov>
2014-08-28 15:43:05 -0500

    automatic inclusion of the env and static files

---
+++ sandbar/static/js/Config.js
@@ -0,0 +1,16 @@
+SB.Config = {};
+/*SB.Config.SITE_PARAMETERS = ['S Sand Cumul Load', 'Discharge']; */
+SB.Config.SITE_PARAMETERS = {
+	cumulSandLoad : {
+		groupName : 'S Sand Cumul Load',
+		colName : 'cumulLoad',
+		description: {} /* Filled in when page is loaded */
+	},
+	discharge : {
+		groupName : 'Discharge',
+		colName: 'discharge',
+		description: {} /* Filled in when page is loaded */
+	}
+};
+
+
