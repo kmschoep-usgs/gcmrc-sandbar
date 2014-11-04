749dfa89274a0dff76c0a50b8adbd32b9844838c
Yan, Andrew N. <ayan@usgs.gov>
2014-11-04 11:58:29 -0600

    removed unused test

--- sandbar/surveys/db_utilities.py
+++ sandbar/surveys/db_utilities.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import cx_Oracle
 from sandbar_project.local_settings import SCHEMA_USER, DB_PWD, DB_NAME
 from .models import Sandbar
 from .db_mappings import AreaVolumeCalcBase
@@ -66,62 +65,6 @@ class AlchemDB(object):
         session = Session()
         
         return session
-
-
-class OracleConnection(object):
-    
-    def __init__(self, schema=SCHEMA_USER, pwd=DB_PWD, db_name=DB_NAME):
-        
-        self.oa = create_cx_oracle_auth_str(schema, pwd, db_name, ip_address=None)
-        
-    def run_oracle_query(self, sql_query, close_connection=True):
-        
-        """
-        Execute a SQL query and returns the results
-        as a list of dictionaries
-        """
-        
-        ora = cx_Oracle.connect(self.oa)
-        cursor = ora.cursor()
-        cursor.execute(sql_query)
-        columns = [desc[0] for desc in cursor.description]
-        record_list_dic = [dict(zip(columns, record)) for record in cursor]
-        if close_connection:
-            ora.close()
-        
-        return (ora, record_list_dic)
-            
-    def execute_dml(self, dml_list, close_connection=True):
-        
-        """
-        Execute a list of SQL commands and report the failures with errors.
-        """
-        
-        failed_list = []
-        
-        ora = cx_Oracle.connect(self.oa)
-        cursor = ora.cursor()
-        sql_statment_count = len(dml_list)
-        for sql_statement in dml_list:
-            try:
-                cursor.execute(sql_statement)
-            except(cx_Oracle.Error), exc:
-                failed_dictionary = {}
-                error, = exc.args
-                failed_dictionary['error'] = error.message
-                failed_dictionary['statement'] = sql_statement
-                failed_list.append(failed_dictionary) 
-                continue
-        cursor.close()
-        ora.commit()
-        if close_connection:
-            ora.close()
-            
-        results = {'total_count': sql_statment_count,
-                   'failed_count': len(failed_list),
-                   'failed_list': failed_list}    
-            
-        return results
 
     
 def create_cx_oracle_auth_str(schema, pwd, db_name, ip_address=None):
