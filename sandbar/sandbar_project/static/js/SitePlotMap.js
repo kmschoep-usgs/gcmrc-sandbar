602e0d3fa5b6797a7b53b067c907997ee48d31c6
kmschoep <kmschoep@usgs.gov>
2015-01-22 12:46:51 -0600

    fix everything that broke when I nuked my environment, including putting the esri-leaflet call in the right place.

--- sandbar/sandbar_project/static/js/SitePlotMap.js
+++ sandbar/sandbar_project/static/js/SitePlotMap.js
@@ -3,13 +3,15 @@ SB.SitePlotMap = function(siteLat,siteLng) {
 	var latlng = L.latLng(parseFloat(siteLng), parseFloat(siteLat));
 	map = L.map('site-loc-map-div', {
 		center: latlng,
-		zoom: 17,
+		zoom: 18,
 		attributionControl: false
 	});
 	
+	//var baseLayer = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png').addTo(map);
 	L.esri.dynamicMapLayer("http://arcweb.wr.usgs.gov/ArcGIS/rest/services/MapServiceImageryMay2009RGB/MapServer", {
 		 opacity: 1
-		 }).addTo(map); 
+	 }).addTo(map); 
+	var marker = L.marker(latlng).addTo(map);
 	//map.setView(latlng, 7);
 	return map;
 };
