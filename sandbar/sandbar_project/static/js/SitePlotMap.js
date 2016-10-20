196e9d14f7d836f00479d8019ea396fd35cd881f
Megan Hines <mhines@usgs.gov>
2016-10-20 13:34:00 -0500

    changing to the new map layer tom gushue sent

--- sandbar/sandbar_project/static/js/SitePlotMap.js
+++ sandbar/sandbar_project/static/js/SitePlotMap.js
@@ -8,7 +8,7 @@ SB.SitePlotMap = function(siteLat,siteLng) {
 	});
 	
 	//var baseLayer = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.png').addTo(map);
-	L.esri.dynamicMapLayer("http://arcweb.wr.usgs.gov/ArcGIS/rest/services/MapServiceImageryMay2009RGB/MapServer", {
+	L.esri.dynamicMapLayer("https://grandcanyon.usgs.gov/arcgis/rest/services/Imagery/IMG_GC_2013_05_4BAND_8B_EARTHDATA/ImageServer", {
 		 opacity: 1
 	 }).addTo(map); 
 	var marker = L.marker(latlng).addTo(map);
